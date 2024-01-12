e912fc7b979dc54b3d618382a3cd6dcf36540d0d
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-01-13 00:54:38 +0200

    Create server.php

---
+++ server.php
@@ -0,0 +1,48 @@
+<?php
+require 'vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = new \Ratchet\Server\IoServer(
+    new \Ratchet\Http\HttpServer(
+        new \Ratchet\WebSocket\WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
+?>
